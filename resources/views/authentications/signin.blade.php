65de4f26ec1d24f67b09a2e937b2e73a690f3867
mohammedboreny <Mohammed.boreny@gmail.com>
2022-10-28 22:51:24 +0200

    Sign in  modified

--- resources/views/authentications/signin.blade.php
+++ resources/views/authentications/signin.blade.php
@@ -1,39 +1,38 @@
 @extends('layout.index')
 @section('content')
-
-    <div class="container justify-content-center">
-        <div class="row justify-content-center">
-             <div class="col-6">
-                 <form>
-                     <img class="mb-4" src="{{asset('images/about-img.png')}}" alt="" width="72" height="57">
-                     <h1 class="h3 mb-3 fw-normal text-center">Please sign in</h1>
-
-                     <div class="form-floating">
-                         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                         <label for="floatingInput">Email address</label>
-                     </div>
-                     <div class="form-floating">
-                         <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                         <label for="floatingPassword">Password</label>
-                     </div>
-
-                     <div class="checkbox mb-3">
-                         <label>
-                             <input type="checkbox" value="remember-me"> Remember me
-                         </label>
-                     </div>
-                     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                     <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
-                 </form>
-             </div>
-            <div class="col-8">
-                <div class="h-100 p-5 bg-light border rounded-3">
-                    <h2>Add borders</h2>
-                    <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
-                    <button class="btn btn-outline-secondary" type="button">Example button</button>
-                </div>
+<div class="container pb-4 pt-4">
+<div class="row featurette">
+    <div class="col-md-7">
+        <form>
+            <h1 class="h3 mb-3 fw-normal"> sign in</h1>
+        
+            <div class="form-floating">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
             </div>
-        </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+            <button class="my-2 btn btn-lg btn-primary" type="submit">Sign in</button>
+            
+          </form>
+            </div>
+    <div class="col text-center">
+        <section >
+            <div class="row">
+              <div class="col pt-5 pl-5">
+                <h1 class="fw-light ">Does not have an account?</h1>
+                <p>
+                  <a href="{{route('signUp')}}" class="btn btn-primary my-2">Sign Up now</a>
+                </p>
+              </div>
+            </div>
+          </section>
     </div>
-
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 @endsection
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -18,7 +18,7 @@
 
     <!-- bootstrap core css -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}" />
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!--owl slider stylesheet -->
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" />
