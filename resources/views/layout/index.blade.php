9c30a1af18e93a9fd15a7712f5325e216347858d
mohammedboreny <Mohammed.boreny@gmail.com>
2022-10-28 22:12:42 +0200

    navbar modified

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Basic -->
     <meta charset="utf-8">
@@ -10,102 +11,111 @@
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <link rel="shortcut icon" href="{{asset('images/fevicon.png')}}" type="">
+    <link rel="shortcut icon" href="{{ asset('images/fevicon.png') }}" type="">
 
     <title> Fruise </title>
 
 
     <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}" />
 
     <!--owl slider stylesheet -->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" />
 
     <!-- font awesome style -->
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" />
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
 
     <!-- Custom styles for this template -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('css/styleSignUp.css')}}">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('css/styleSignUp.css') }}">
     <!-- responsive style -->
-    <link href="{{asset('css/responsive.css')}}" rel="stylesheet" />
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" />
 
 </head>
 
 <body>
 
-<div class="hero_area">
-    <div class="hero_bg_box">
-        <img src="{{asset('images/slider-bg.jpg')}}" alt="">
-    </div>
-    <!-- header section strats -->
-    <header class="header_section">
-        <div class="header_top">
-            <div class="container-fluid ">
-                <div class="header_top_content">
-                    <a class="navbar-brand d-none d-lg-flex" href="index.html">
-              <span>
-                Fruise
-              </span>
-                    </a>
-                    <div class="contact_nav">
-                        <a href="">
-                            <i class="fa fa-phone" aria-hidden="true"></i>
+    <div class="hero_area">
+        <div class="hero_bg_box">
+            <img src="{{ asset('images/slider-bg.jpg') }}" alt="">
+        </div>
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="header_top">
+                <div class="container-fluid ">
+                    <div class="header_top_content">
+                        <a class="navbar-brand d-none d-lg-flex" href="index.html">
                             <span>
-                  +01 123455678990
-                </span>
+                                Fruise
+                            </span>
                         </a>
-                        <a href="">
-                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                            <span>
-                  demo@gmail.com
-                </span>
-                        </a>
+                        <div class="contact_nav">
+                            <a href="">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span>
+                                    +01 123455678990
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <span>
+                                    demo@gmail.com
+                                </span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="header_bottom">
-            <div class="container-fluid">
-                <nav class="navbar navbar-expand-lg custom_nav-container ">
-                    <a class="navbar-brand d-flex d-lg-none" href="index.html">
-              <span>
-                Fruise
-              </span>
-                    </a>
+            <div class="header_bottom">
+                <div class="container-fluid">
+                    <nav class="navbar navbar-expand-lg  custom_nav-container ">
+                        <a class="navbar-brand d-flex d-lg-none " href="{{ route('home') }}">
+                            <span>
+                                Fruise
+                            </span>
+                        </a>
 
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class=""> </span>
-                    </button>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class=""> </span>
+                        </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav  ">
-                            <li class=" nav-item {{ (request()->is('home')) ? 'active' : '' }}">
-                                <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item {{ (request()->is('service')) ? 'active' : '' }}">
-                                <a class="nav-link" href="{{ route('service') }}">Services</a>
-                            </li>
-                            <li class="nav-item {{ (request()->is('about')) ? 'active' : '' }}">
-                                <a class="nav-link" href="{{ route('about') }}"> About</a>
-                            </li>
-                            <li class="nav-item {{ (request()->is('contact')) ? 'active' : '' }}">
-                                <a class="nav-link" href="{{ route('contact') }}">Contact Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i> Login</a>
-                            </li>
-                            <form class="form-inline">
-                                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                                    <i class="fa fa-search" aria-hidden="true"></i>
-                                </button>
-                            </form>
-                        </ul>
-                    </div>
-                </nav>
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                            <ul class="navbar-nav ">
+                                <li class=" nav-item {{ request()->is('home') ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('home') }}">Home <span
+                                            class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item {{ request()->is('service') ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('service') }}">Services</a>
+                                </li>
+                                <li class="nav-item {{ request()->is('about') ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('about') }}"> About</a>
+                                </li>
+                                <li class="nav-item {{ request()->is('contact') ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('contact') }}">Contact Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i>
+                                        Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#"> <i class="fa fa-user" aria-hidden="true"></i>
+                                        Sign Up</a>
+                                </li>
+                                {{-- <form class="form-inline">
+                                    <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                                        <i class="fa fa-search" aria-hidden="true"></i>
+                                    </button>
+                                </form> --}}
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
     </div>
     <!-- end header section -->
 
@@ -114,7 +124,7 @@
 
 
 
-@yield('content')
+    @yield('content')
 
 
 
@@ -136,7 +146,8 @@
                             About
                         </h4>
                         <p>
-                            Necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful
+                            Necessary, making this the first true generator on the Internet. It uses a dictionary of
+                            over 200 Latin words, combined with a handful
                         </p>
                         <div class="info_social">
                             <a href="">
@@ -163,20 +174,20 @@
                             <a href="">
                                 <i class="fa fa-map-marker" aria-hidden="true"></i>
                                 <span>
-                  Location
-                </span>
+                                    Location
+                                </span>
                             </a>
                             <a href="">
                                 <i class="fa fa-phone" aria-hidden="true"></i>
                                 <span>
-                  Call +01 1234567890
-                </span>
+                                    Call +01 1234567890
+                                </span>
                             </a>
                             <a href="">
                                 <i class="fa fa-envelope" aria-hidden="true"></i>
                                 <span>
-                  demo@gmail.com
-                </span>
+                                    demo@gmail.com
+                                </span>
                             </a>
                         </div>
                     </div>
@@ -219,17 +230,17 @@
     <!-- footer section -->
 
     <!-- jQery -->
-    <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
     <!-- popper js -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
     <!-- bootstrap js -->
-    <script src="{{asset('js/bootstrap.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
     <!-- owl slider -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <!-- custom js -->
-    <script src="{{asset('js/custom.js')}}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
     <!-- Google Map -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
     </script>
